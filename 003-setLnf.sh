3c09a2e57fa37e11ae4f9afe05ac9db7885878eb
Axel Philipsenburg <axel@wired-space.de>
2013-12-18 20:26:32 +0100

    Install themes in system instead of user directory space. Needed for sudo'ed GUI applications to pick up the theme.

--- 003-setLnf.sh
+++ 003-setLnf.sh
@@ -17,12 +17,16 @@ echo Setting up themes ...
 ## New place for Gnome 3.10
 mkdir -p ~/.local/share/themes
 ## Old place. Link to new. May show duplicate entries in tweak tool.
-ln -sf ~/.local/share/themes/ ~/.themes
+THEMES_DIR="/usr/share/themes/"
 ZUKITWO_UNPACK="/tmp/zukitwo-unpack"
 mkdir -p ${ZUKITWO_UNPACK}
-unzip -d /tmp/zukitwo-unpack assets/zukitwo_by_lassekongo83-d3df2ot.zip
-cp -r ${ZUKITWO_UNPACK}/Zukitwo* ~/.local/share/themes/
+unzip -d ${ZUKITWO_UNPACK} assets/zukitwo_by_lassekongo83-d3df2ot.zip
+sudo cp -r ${ZUKITWO_UNPACK}/Zukitwo* ${THEMES_DIR}
+sudo chown -R root:root ${THEMES_DIR}/Zukitwo* 
+sudo find ${THEMES_DIR} -type d -exec chmod 775 {} \;
 rm -r ${ZUKITWO_UNPACK}
 /usr/bin/dconf write /org/gnome/desktop/interface/gtk-theme "'Zukitwo'"
+/usr/bin/dconf write /org/gnome/desktop/wm/preferences/theme "'Zukitwo'"
 
 echo Done setting up Look\'n feel.
+
